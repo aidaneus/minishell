e84f23c4637a9834e124e7bae2bcdd7977823ffa
Gormon Bump <gbump@mi-k3.msk.21-school.ru>
2021-09-16 02:39:25 +0300

    norminette

--- process/export_add.c
+++ process/export_add.c
@@ -6,7 +6,7 @@
 /*   By: gbump <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 01:21:38 by gbump             #+#    #+#             */
-/*   Updated: 2021/09/16 01:21:43 by gbump            ###   ########.fr       */
+/*   Updated: 2021/09/16 02:38:43 by gbump            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ static void	add_free_and_malloc(t_parser *parser, char **line, int a, int c1)
 	if (parser->env[parser->len] && c1 == 'c')
 		free(parser->env[parser->len]);
 	if (c1 == 'c')
-		parser->env[parser->len] = (char*)malloc(sizeof(char) * (ft_strlen(line[a]) + 1));
+		parser->env[parser->len] = (char *)malloc(sizeof(char)
+				* (ft_strlen(line[a]) + 1));
 	parser->export[parser->len] = (char *)malloc(sizeof(char)
 			* (ft_strlen(line[a]) + 3));
 }
@@ -28,7 +29,7 @@ int	export_add(int a, t_parser *parser, char **line, int c1)
 {
 	int	b;
 	int	c;
-	int en;
+	int	en;
 
 	b = 0;
 	c = 0;
@@ -38,7 +39,7 @@ int	export_add(int a, t_parser *parser, char **line, int c1)
 	{
 		parser->export[parser->len][b] = line[a][c];
 		 if (c1 == 'c')
-		 	parser->env[parser->len][en] = line[a][c];
+			parser->env[parser->len][en] = line[a][c];
 		if (line[a][c] == '=')
 			parser->export[parser->len][++b] = '"';
 		if (line[a][c + 1] == '\0' && b > c)
