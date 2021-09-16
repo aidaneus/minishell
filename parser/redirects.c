fb86551a61d14200de79ac7edb32cf5b599e4836
Nidia Hildred <nhildred@mi-k5.msk.21-school.ru>
2021-09-16 03:06:56 +0300

    heredoc process

--- parser/redirects.c
+++ parser/redirects.c
@@ -22,18 +22,28 @@ void	close_fd(t_parser *parser)
 			close(parser->oldstdout);
 		}
 	}
-	if (parser->flag_close == 2)
+	if (parser->flag_close == 2 && parser->flag_redirect != 1)
 		close(parser->fd2);
+}
+
+void	check_red(char *line, int *a, t_parser *parser, int num)
+{
+	if (line[*a] == '>' && num == 1)
+	{
+		parser->flag_redirect = 1;
+		(*a)++;
+	}
+	if (line[*a] == '<' && num == 2)
+	{
+		parser->flag_redirect = 1;
+		(*a)++;
+	}
 }
 
 void	red_output(char *line, int *a, char *tmp, t_parser *parser)
 {
 	parser->fd = 0;
-	if (line[*a] == '>')
-	{
-		parser->flag_redirect = 1;
-		(*a)++;
-	}
+	check_red(line, a, parser, 1);
 	skip_spaces(line, a);
 	while (line[*a] != ' ' && line[*a] != '\0')
 		tmp = ft_strjoin_char(tmp, line[(*a)++]);
@@ -54,9 +64,15 @@ void	red_input(char *line, int *a, char *tmp, t_parser *parser)
 	int		output;
 	char	*lines;
 
+	check_red(line, a, parser, 2);
 	skip_spaces(line, a);
 	while (line[*a] != ' ' && line[*a] != '\0')
 		tmp = ft_strjoin_char(tmp, line[(*a)++]);
+	if (parser->flag_redirect == 1)
+	{
+		parser->flag_heredoc = heredoc(tmp);
+		return ((void) NULL);
+	}
 	parser->fd2 = open(tmp, O_RDONLY, 0644);
 	output = get_next_line(parser->fd2, &lines);
 	while (output > 0)
@@ -83,4 +99,10 @@ void	redirect(char *line, int *a, int num, t_parser *parser)
 		red_output(line, a, tmp, parser);
 	if (num == 2 && parser->line[0] == NULL)
 		red_input(line, a, tmp, parser);
+	if (parser->flag_heredoc == 1)
+	{
+		line = "";
+		unlink(".heredoc");
+		exit(0);
+	}
 }
